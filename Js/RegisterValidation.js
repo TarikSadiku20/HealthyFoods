6152434f7aaea1d3a8f0718850ee84d0ea6e6ff1
tariksadiku <tariksadiku10@gmail.com>
2021-01-10 17:01:58 +0100

    Added slider

---
+++ Js/RegisterValidation.js
@@ -0,0 +1,13 @@
+const name = document.querySelector(".username");
+const surname = document.querySelector(".surname");
+const email = document.querySelector(".email");
+const password = document.querySelector(".password");
+
+
+
+document.querySelector("form").addEventListener("submit", function(e){
+   e.preventDefault();
+})
+
+
+
