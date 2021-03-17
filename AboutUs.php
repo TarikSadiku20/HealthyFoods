eb1d2d0ce694956a40788d79bb3fdb46772e6102
ak44374 <77198256+ak44374@users.noreply.github.com>
2021-03-17 22:36:06 +0100

    Update AboutUs.php

--- AboutUs.php
+++ AboutUs.php
@@ -15,71 +15,35 @@
 
 
     <main>
-
-        
-        <div class="banner-img"></div>
-        <div class="banner-text">
-            <h1>
-                Pioonering healthy choices in foods since the 00's
-            </h1>
-            
-        </div>
-
-
-        <section class="secondsection">
-
-            
-            
-             
-            <div>
-                <p>
-                    Calorie, macronutrient and </br> micronutrient </br>tracking in </br>the selected foods of yours.</br> Planning ahead, looking</br> behind as to what you ate.
-    
-                </p>
-            </div>
-            
-
-            <div>
-                <p>Selecting many famous diets like: </br> Ketogenic, Pescetarian,</br> Vegetarian, Vegan, or </br>custom diet specific</br> to your choosing. </p>
-
-            </div>
-
-            <div>
-
+        <div class="about-section">
                 
-                <p>
-                A general education into </br> the food that goes </br> into your body, what is </br>truly bad  </br>and what is truly good.
-
-            </p>
-
+                    <div class="box" id="1">
+                        <img src="../images/tarik.jpg" alt="" class="box-img">
+                        <h1>
+                      Tarik Sadiku</h1>
+                      <h5>
+                      </br>
+                      Web Developer - Web Designer</h5>
+                      <p>
+                    </br>
+                          CEO at 
+                      </p>
+                    </div>
+                    <div class="box" id="2">
+                      <img src="../images/pic.jpeg" alt="" class="box-img">
+                        <h1>
+                      Arlind Kozmaqi</h1>
+                      <h5>
+                      </br>
+                      Web Developer - Web Designer</h5>
+                      <p>
+                      </br>
+                          Creator / CoFounder
+                      </p>
+                      </div>
+                
             </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            
-        </section>
-
-        
-
+        </div>    
     </main>
 
 
